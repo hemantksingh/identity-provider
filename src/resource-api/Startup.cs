1bff245184f101a81589187ced840d8ce34f0765
Hemant Kumar <hemantksingh.hk@gmail.com>
2018-01-18 11:34:21 +0000

    Remove Identity server dep on api authentication

--- src/resource-api/Startup.cs
+++ src/resource-api/Startup.cs
@@ -1,44 +1,58 @@
-﻿using IdentityServer4.AccessTokenValidation;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace resource_api
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-	        services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
-		        .AddIdentityServerAuthentication(options =>
-		        {
-			        options.Authority = "https://localhost:44376/";
-			        options.RequireHttpsMetadata = true;
-			        options.ApiName = "resourceapi";
-		        });
-				
-            services.AddMvc();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			const string authenticationScheme = "JwtBearer";
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddAuthentication(options =>
+				{
+					options.DefaultAuthenticateScheme = authenticationScheme;
+					options.DefaultChallengeScheme = authenticationScheme;
+				})
+				.AddJwtBearer(authenticationScheme, jwtBearerOptions =>
+					{
+						jwtBearerOptions.Authority = "https://localhost:44376/";
+						jwtBearerOptions.RequireHttpsMetadata = true;
+						jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+						{
+							ValidateIssuer = true,
+							ValidIssuer = "https://localhost:44376/",
+							ValidAudience = "resourceapi",
+							ValidateAudience = true,
+							ValidateLifetime = true
+						};
+					}
+				);
 
-	        app.UseAuthentication();
-            app.UseMvc();
-        }
-    }
+			services.AddMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseAuthentication();
+			app.UseMvc();
+		}
+	}
 }
