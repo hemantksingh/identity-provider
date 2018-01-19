2e6345701e481e1e0d1f59e342a2c53dc421793c
Hemant Kumar <hemantksingh.hk@gmail.com>
2018-01-19 15:55:06 +0000

    Configure Serilog

--- src/client-webapp/Program.cs
+++ src/client-webapp/Program.cs
@@ -1,25 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace client_webapp
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+	public class Program
+	{
+		public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+				optional: true)
+			.AddEnvironmentVariables()
+			.Build();
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+		public static int Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.ReadFrom.Configuration(Configuration)
+				.Enrich.FromLogContext()
+				.WriteTo.Console()
+				.CreateLogger();
+
+			try
+			{
+				Log.Information("Starting things off...");
+
+				BuildWebHost(args).Run();
+
+				return 0;
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "Host terminated unexpectedly");
+				return 1;
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}		
+		}
+
+		public static IWebHost BuildWebHost(string[] args)
+		{
+			return WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.UseConfiguration(Configuration)
+				.ConfigureLogging(builder =>
+				{
+					builder.ClearProviders();
+					builder.AddSerilog();
+				})
+				.Build();
+		}
+	}
 }
