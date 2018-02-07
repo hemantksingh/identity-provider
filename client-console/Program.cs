c050aae95de0e25793e33c7e4c5ed46d196b36ab
David Pendray <david.pendray@kalibrate.com>
2018-02-07 10:00:47 +0000

    Added client-console
    Co-authored-by: Janet Li <35925625+JanetLiKB@users.noreply.github.com>

---
+++ client-console/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace client_console
+{
+    public class Program
+    {
+        public static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("https://localhost:44376");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("resourceapi");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("https://localhost:44357/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.Read();
+        }
+    }
+}
--- src/identity-provider/SeedData.cs
+++ src/identity-provider/SeedData.cs
@@ -115,6 +115,21 @@ namespace identity_provider
 		                IdentityServerConstants.StandardScopes.Profile,
 		                "resourceapi"
                     }
+		        },new Client
+		        {
+		            ClientId = "client",
+
+		            // no interactive user, use the clientid/secret for authentication
+		            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+		            // secret for authentication
+		            ClientSecrets =
+		            {
+		                new Secret("secret".Sha256())
+		            },
+
+		            // scopes that client has access to
+		            AllowedScopes = { "resourceapi" }
 		        }
 
 
