69dda510d258656401ae7c88fcc7431699105907
David Pendray <david.pendray@kalibrate.com>
2018-02-09 15:54:59 +0000

    Add api-gateway project
    Co-authored-by: Janet Li <35925625+JanetLiKB@users.noreply.github.com>

---
+++ api-gateway/FixHeadersHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace api_gateway
+{
+    public class FixHeadersHandler : HttpClientHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes("administrator: administrator")));
+            var response = await base.SendAsync(request, cancellationToken);
+            response.Headers.Remove("Access-Control-Allow-Origin");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return response;
+        }
+    }
+}
---
+++ api-gateway/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace api_gateway
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "https://localhost:44376/";
+                    options.RequireHttpsMetadata = true;
+                    options.ApiName = "resourceapi";
+                });
+
+            services.AddMvc();
+
+            services.AddCors(options =>
+            {
+                // this defines a CORS policy called "default"
+                options.AddPolicy("default", policy =>
+                {
+                    policy.WithOrigins("http://localhost")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors("default");
+            app.UseAuthentication();
+
+            app.MapWhen(h => true, builder => builder.RunProxy(new ProxyOptions
+            {
+                Scheme = "http",
+                Host = "localhost",
+                Port = "80",
+                BackChannelMessageHandler = new FixHeadersHandler()
+            }));
+
+        }
+    }
+}
