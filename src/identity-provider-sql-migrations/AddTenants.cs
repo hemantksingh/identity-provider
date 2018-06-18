dcf7c612e50803953742fe7e9b1f3fbc79fe5d05
Hemant Kumar <hemantksingh.hk@gmail.com>
2018-06-18 01:53:03 +0100

    Add tenant db migration

---
+++ src/identity-provider-sql-migrations/AddTenants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentMigrator;
+
+namespace identity_provider_sql_migrations
+{
+	[Migration(20180618011400)]
+	public class AddTenants : Migration
+	{
+		public override void Up()
+		{
+			Create.Table("Tenants")
+				.WithColumn("TenantId").AsString(64).NotNullable().PrimaryKey()
+				.WithColumn("TenantName").AsString(64).NotNullable().Unique()
+				.WithColumn("OrganizationName").AsString(100).NotNullable()
+				.WithColumn("License").AsString(255).Nullable()
+				.WithColumn("IsActive").AsBoolean().NotNullable();
+
+			Alter.Table("Users").AddColumn("TenantId").AsString(64);
+
+			Create.ForeignKey("FK_Users_Tenants_TenantId")
+				.FromTable("Users").ForeignColumn("TenantId")
+				.ToTable("Tenants").PrimaryColumn("TenantId");
+		}
+
+		public override void Down()
+		{
+			Delete.ForeignKey("FK_Users_Tenants_TenantId");
+			Delete.Column("TenantId").FromTable("Users");
+			Delete.Table("Tenants");
+		}
+	}
+}
--- src/identity-provider-sql-migrations/AlterUsers.cs
+++ src/identity-provider-sql-migrations/AlterUsers.cs
@@ -2,22 +2,21 @@
 
 namespace identity_provider_sql_migrations
 {
-	[Migration(2)]
-	public class AlterUsers: Migration
+	[Migration(20180127001104)]
+	public class AddUserClaims: Migration
     {
 	    public override void Up()
 	    {
 		    Alter.Table("UserClaims").AlterColumn("Type").AsString(100);
 		    Alter.Table("UserClaims").AlterColumn("Value").AsString(255);
-		    Create.UniqueConstraint("uk_Users_Username")
-				.OnTable("Users").Column("Username");
+		    Alter.Table("Users").AlterColumn("Username").AsString(100).NotNullable().Unique();
 		}
 
 	    public override void Down()
 	    {
 		    Alter.Table("UserClaims").AlterColumn("Type").AsString(250);
 			Alter.Table("UserClaims").AlterColumn("Value").AsString(250);
-			Delete.UniqueConstraint("uk_Users_Username").FromTable("Users");
+		    Alter.Table("Users").AlterColumn("Username").AsString(100).NotNullable();
 	    }
     }
 }
