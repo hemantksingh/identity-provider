858f4f8d3cce4b00bd747cedbc4c36b3a229f5c2
Hemant Kumar <hemantksingh.hk@gmail.com>
2018-06-18 01:54:22 +0100

    Fix rename

--- src/identity-provider-sql-migrations/AlterUsers.cs
+++ src/identity-provider-sql-migrations/AlterUsers.cs
@@ -3,20 +3,20 @@
 namespace identity_provider_sql_migrations
 {
 	[Migration(20180127001104)]
-	public class AddUserClaims: Migration
+	public class AlterUsers: Migration
     {
 	    public override void Up()
 	    {
+		    Alter.Table("Users").AlterColumn("Username").AsString(100).NotNullable().Unique();
 		    Alter.Table("UserClaims").AlterColumn("Type").AsString(100);
 		    Alter.Table("UserClaims").AlterColumn("Value").AsString(255);
-		    Alter.Table("Users").AlterColumn("Username").AsString(100).NotNullable().Unique();
-		}
+	    }
 
 	    public override void Down()
 	    {
-		    Alter.Table("UserClaims").AlterColumn("Type").AsString(250);
-			Alter.Table("UserClaims").AlterColumn("Value").AsString(250);
 		    Alter.Table("Users").AlterColumn("Username").AsString(100).NotNullable();
+		    Alter.Table("UserClaims").AlterColumn("Type").AsString(250);
+		    Alter.Table("UserClaims").AlterColumn("Value").AsString(250);
 	    }
     }
 }
