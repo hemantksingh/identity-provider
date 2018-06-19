e2ffd7bb702061f22f371bf14610014ed39bc559
Hemant Kumar <hemantksingh.hk@gmail.com>
2018-06-20 00:54:51 +0100

    Move dependency initialisation

---
+++ src/identity-provider/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+using identity;
+using identity_provider.Tenants;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace identity_provider
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddRepositories(this IServiceCollection services,
+			string connectionString)
+		{
+			services.AddSingleton(provider => new UserRepository(() =>
+				GetConnection(connectionString), () => new UnitOfWork(GetConnection(connectionString))));
+			services.AddSingleton(provider => new TenantRepository(() =>
+				GetConnection(connectionString)));
+
+			return services;
+		}
+
+		/// <summary>
+		/// Creates a new sql connection per query. Closing the connection after finishing processing the query
+		/// does not actually close the physical connection but just returns it to the connection pool, managed
+		/// by dotnet. Let dotnet handle connection pooling:
+		/// https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/sql-server-connection-pooling
+		/// </summary>
+		/// <returns>SqlConnection</returns>
+		private static SqlConnection GetConnection(string connectionString)
+		{
+			var sqlConnection = new SqlConnection(connectionString);
+			sqlConnection.Open();
+			return sqlConnection;
+		}
+	}
+}
--- src/identity-provider/Startup.cs
+++ src/identity-provider/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.SqlClient;
-using System.Linq;
+﻿using System.Linq;
 using identity;
 using identity_provider.Tenants;
 using Microsoft.AspNetCore.Builder;
@@ -15,9 +14,7 @@ namespace identity_provider
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			const string connectionString = @"Server=localhost;Database=identity;Data Source=.;Initial Catalog=identity;Integrated Security=True";
-		    services.AddSingleton(provider => new UserRepository(() => GetConnection(connectionString), () => new UnitOfWork(GetConnection(connectionString))));
-			services.AddSingleton(provider => new TenantRepository(() => GetConnection(connectionString)));
+			services.AddRepositories(@"Server=localhost;Database=identity;Data Source=.;Initial Catalog=identity;Integrated Security=True");
 
 			services.AddMvc();
 			
@@ -27,20 +24,7 @@ namespace identity_provider
 				.AddInMemoryClients(SeedData.GetClients())
 				.AddInMemoryIdentityResources(SeedData.GetIdentityResources())
 				.AddInMemoryApiResources(SeedData.GetApiResources());
-		}
 
-		/// <summary>
-		/// Creates a new sql connection per query. Closing the connection after finishing processing the query
-		/// does not actually close the physical connection but just returns it to the connection pool, managed
-		/// by dotnet. Let dotnet handle connection pooling:
-		/// https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/sql-server-connection-pooling
-		/// </summary>
-		/// <returns>SqlConnection</returns>
-		private static SqlConnection GetConnection(string connectionString)
-		{
-			var sqlConnection = new SqlConnection(connectionString);
-			sqlConnection.Open();
-			return sqlConnection;
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
